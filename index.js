42af50a388d19245054ec87f68299cee770f163b
Kayla Lebron <kayla.lebron12@gmail.com>
2023-05-05 11:48:35 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result
+  if (someNumber < 400) {
+    result = "This one is on me!"
+  }
+  else if (someNumber < 2000) {
+    result = 'That will be twenty bucks.'
+  }
+  else if (someNumber < 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else {
+    result = 'No can do.'
+  }
+ 
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+return (city === "NYC") ? 'Ok, sounds good.' : "No go.";
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let answer
+  switch(tip){
+    case 'generous':
+      answer = "Thank you so much.";
+      break;
+    case 'not as generous':
+      answer = 'Thank you.';
+      break;
+    default:
+      answer = 'Bye.'
+  }
+  return answer
 }
